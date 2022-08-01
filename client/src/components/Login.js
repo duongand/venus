f7d6a49e6713f6a17b3a270170c038e77770f3fb
duongand <duonganderson@gmail.com>
2022-08-01 11:33:12 -0400

    tooltip addition

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,7 +3,7 @@ function Login() {
         <div className="login-page">
             <h1 className="login-page--title">venus</h1>
             <button className="login-page--login-button">
-                <a className="login-page--login-text" href="http://localhost:8888/login">Login with Spotify</a>
+                <a className="login-page--login-text" href="https://react-spotify-venus.herokuapp.com/login">Login with Spotify</a>
             </button>
         </div>
     );
